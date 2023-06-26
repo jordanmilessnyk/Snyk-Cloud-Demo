8eadedb475dac5c24a5bc2be7f6c4e8ef1d643bf
jordaniac89 <jordan.k.miles@gmail.com>
2023-06-26 11:42:02 -0400

    Revert "adding tests"
    This reverts commit 23f5ec0b1695410ee5f0fabdc234447dee1d8603.

--- lib/relations.rego
+++
@@ -1,3 +0,0 @@
-package relations
-
-import data.snyk
--- spec/rules/EC2_ALLOWED_TYPES/inputs/EC2_ALLOWED_TYPES.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "valid" {
-  instance_type = "t2.medium"
-}
-
-resource "aws_instance" "invalid" {
-  instance_type = "t2.micro"
-}
